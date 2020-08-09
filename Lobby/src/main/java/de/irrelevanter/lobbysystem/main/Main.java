3dd0069bf2e5fa955e2be7a93242eb87b501eac1
Irrelevanter <69420104+Irrelevanter@users.noreply.github.com>
2020-08-09 12:49:36 +0200

    Add files via upload

---
+++ Lobby/src/main/java/de/irrelevanter/lobbysystem/main/Main.java
@@ -0,0 +1,14 @@
+package de.irrelevanter.lobbysystem.main;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * JavaDoc this file!
+ * Created: 07.08.2020
+ *
+ * @author WeLoveSpigotPlugins (welovespigotplugins@gmail.com)
+ */
+public class Main extends JavaPlugin {
+    Player
+}
